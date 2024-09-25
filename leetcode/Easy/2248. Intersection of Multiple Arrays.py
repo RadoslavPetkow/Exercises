796e737714a82ee3aa8bcaf6d7ba86ff21a7d098
Radoslav Petkov <156192995+RadoslavPetkow@users.noreply.github.com>
2024-09-25 20:46:37 +0300

    Delete leetcode/Easy directory

--- leetcode/Easy/2248. Intersection of Multiple Arrays.py
+++
@@ -1,9 +0,0 @@
-def intersection_of_arrays(nums):
-    intersection_set = set(nums[0])
-
-    for arr in nums[1:]:
-        intersection_set &= set(arr)
-
-    result = sorted(intersection_set)
-
-    return result
--- leetcode/Easy/349. Intersection of Two Arrays.py
+++
@@ -1,4 +0,0 @@
-nums1 = [1,2,3,3]
-nums2 = [1,1,2,2]
-
-print([list(set(nums1) - set(nums2)),list(set(nums2) - set(nums1))])
